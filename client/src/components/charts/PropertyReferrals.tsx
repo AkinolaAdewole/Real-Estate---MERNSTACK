4df6757656ef6652bde7af5e8ee60cc21e0573fb
Akinola <adewoleakinola24@gmail.com>
2023-08-04 19:30:33 +0100

    Design using Material UI

--- client/src/components/charts/PropertyReferrals.tsx
+++ client/src/components/charts/PropertyReferrals.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import {Box, Stack, Typography} from '@mui/material';
+import { PropertyReferralsInfo } from 'constants';
+
+interface ProgressBarProps {
+  title: string;
+  percentage: number;
+  color: string;
+}
 
 const PropertyReferrals = () => {
   return (
-    <>Referrals</>
+    <Box id='chart' p={4} flex={1} bgcolor='#fcfcfc' minHeight='400px'
+       display='flex' flexDirection="column" borderRadius="15px" ></Box>
   )
 }
 
