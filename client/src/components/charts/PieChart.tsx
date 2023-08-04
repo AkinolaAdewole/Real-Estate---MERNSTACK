960a7e0be9818387d580dbc9bb2e0f549dbdbf1d
Akinola <adewoleakinola24@gmail.com>
2023-08-04 16:52:40 +0100

    CLIENT SIDE DEVELOPMENT

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
-interface PieChartProps {
-  title: string;
-}
+import{Box, Typography, Stack} from '@mui/material';
+import { PieChartProps } from 'interfaces/Home';
 
-const PieChart: React.FC<PieChartProps> = ({ title }) => {
+const PieChart = ({
+  title, value, series, colors
+}:PieChartProps) => {
   return (
-    <div>PieChart</div>
+    <Box>
+      <Stack direction="column">
+        <Typography fontSize={14}
+         color="#808191">
+             {title} 
+          </Typography>
+
+        <Typography fontSize={14} color='#11142d'
+          fontWeight={700} mt={1}>
+             {value}
+          </Typography>
+      </Stack>
+    </Box>
   );
 };
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,6 +4,8 @@ import Typography from '@mui/material/Typography';
 import Box from "@mui/material/Box"
 import  Stack  from '@mui/material/Stack';
 
+import { PieChartProps } from 'interfaces/Home';
+
 import {
   PieChart,
   PropertyReferrals,
@@ -24,13 +26,32 @@ const Home = () => {
           <Box mt='20px' display='flex'
            flexWrap='wrap' gap={4}>
 
-                <PieChart 
-                   title='Properties for sale'
+<PieChart
+                    title="Properties for Sale"
                     value={684}
                     series={[75, 25]}
-                    color={['#475be8', '#e4e8ef']}
-                    />
+                    colors={["#275be8", "#c4e8ef"]}
+                />
+                <PieChart
+                    title="Properties for Rent"
+                    value={550}
+                    series={[60, 40]}
+                    colors={["#275be8", "#c4e8ef"]}
+                />
+                <PieChart
+                    title="Total customers"
+                    value={5684}
+                    series={[75, 25]}
+                    colors={["#275be8", "#c4e8ef"]}
+                />
+                <PieChart
+                    title="Properties for Cities"
+                    value={555}
+                    series={[75, 25]}
+                    colors={["#275be8", "#c4e8ef"]}
+                />
                     
+
            </Box>
 
         </Box>
