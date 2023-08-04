f8a4588079c8ea57fc137e8a61d27151758190bd
Akinola <adewoleakinola24@gmail.com>
2023-08-04 12:42:58 +0100

    CLIENT SIDE DEVELOPMENT

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,26 @@
-export { Header } from "./header/Header";
+//Common
+import Profile from "./common/Profile";
+import PropertyCard from "./charts/PropertyCard";
+import CustomButton from "./common/CustomButton";
+
+// Charts
+import PieChart from "./charts/PieChart";
+import PropertyReferrals from "./charts/PropertyReferrals";
+import TotalRevenue from "./charts/TotalRevenue";
+
+//Agent
+import AgentCard from "./Agent/AgentCard";
+
+//Home
+import Topagent from "./home/Topagent";
+
+export {
+    Profile,
+    PropertyCard,
+    CustomButton,
+    PieChart,
+    PropertyReferrals,
+    TotalRevenue,
+    AgentCard,
+    Topagent
+}
