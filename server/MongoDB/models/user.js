2dbf16ff632adafae46fb90415d6b21e97764019
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-07 23:11:07 +0100

    Real Estate Management backend development

--- server/MongoDB/models/user.js
+++ server/MongoDB/models/user.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const Userschema= new mongoose.Schema({
+const UserSchema= new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     avatar: { type: String, required: true },
