b1092190b51491147fb1ba11e025f65526f23435
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-07 17:33:36 +0100

    Real Estate Management design using Material UI Design

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import TextField from "@mui/material/TextField";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Stack from "@mui/material/Stack";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
 
-const Form = () => {
+import { FormProps } from 'interfaces/common';
+import CustomButton from './CustomButton';
+
+const Form = ({}) => {
   return (
     <div>Form</div>
   )
--- client/src/pages/CreateProperty.tsx
+++ client/src/pages/CreateProperty.tsx
@@ -3,9 +3,29 @@ import { useGetIdentity } from '@refinedev/core'
 import { FieldValue, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import Form from 'components/common/Form';
+
 const CreateProperty = () => {
+  const navigate = useNavigate();
+  const {data:user}= useGetIdentity();
+  const[propertyImage, setPropertyImage]=useState({
+                  name:'',
+                     url:'' });
+
+  const {refineCore:{onFinish, formLoading}, register, handleSubmit}=useForm();
+
+  const handleImageChange=()=>{};
+  const onFinishHandler=()=>{};
   return (
-    <div>CreateProperty</div>
+      <Form
+      type="create"
+      register={register}
+      onFinsh={onFinish}
+      formLoading={formLoading}
+      handleSubmit={handleSubmit}
+      propertyImage={propertyImage}
+      handleImageChange={handleImageChange}
+      onFinishHandler={onFinishHandler}
+      />
   )
 }
 
