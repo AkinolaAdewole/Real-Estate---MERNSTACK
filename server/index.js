7fcd5f24091ef8e9f0da8da9fcfac0f7ac975d8a
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-07 22:17:32 +0100

    Real Estate Management backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import  express  from "express";
 import * as dotenv  from 'dotenv';
 import cors from 'cors';
 
+import connectDB from "./MongoDB/connect.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,8 +12,21 @@ app.use(express.json({limit:'50mb'}));
 
 app.get('/', (req,res)=>{
     res.send({message:"Hello world"});
-})
+});
 
 
 const port= 4200;
+const startserver= async()=>{
+    try {
+        connectDB(process.env.MongoDB_URL)
+        
+        app.listen(port,()=>{
+            console.log('connected');
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+startserver();
 
