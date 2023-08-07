8818980dc6b4bdb1c67432132e765346a5364488
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-07 17:00:46 +0100

    Real Estate Management design using Material UI Design

--- client/src/pages/CreateProperty.tsx
+++ client/src/pages/CreateProperty.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { useGetIdentity } from '@refinedev/core'
+import { FieldValue, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import Form from 'components/common/Form';
 const CreateProperty = () => {
   return (
     <div>CreateProperty</div>
