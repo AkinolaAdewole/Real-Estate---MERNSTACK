70dfffa950c1aa85d71e4efc80e99e319c452d5b
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-07 15:04:17 +0100

    Material UI Design

--- client/src/pages/All-Properties.tsx
+++ client/src/pages/All-Properties.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Add, RemoveFromQueue } from '@mui/icons-material'
+import { useList } from '@refinedev/core'
+import {Box, Stack, Typography} from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 const AllProperties = () => {
   return (
